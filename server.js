d51081c083f0dffb6047eec5c55d6f663aabd31d
IshitaG02 <Ishita.Gupta@kalvium.community>
2025-04-12 15:08:49 +0530

    Moved backend to root for Render deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const cafeRouter = require("./routes/cafes");
+const cafeRouter = require("./backend/routes/cafes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
