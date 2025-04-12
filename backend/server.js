894fef3e8ea2c9393931ec63be9711e5b813b5e0
IshitaG02 <Ishita.Gupta@kalvium.community>
2025-04-12 16:13:48 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const cafeRouter = require("./backend/routes/cafes");
+const cafeRouter = require('./routes/cafes');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
